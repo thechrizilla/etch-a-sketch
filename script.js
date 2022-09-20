473ea0abd95730092a3be57af332849a54b02080
Chris Chan <chrisck.chan@hotmail.com>
2022-09-20 13:30:30 -0400

    finish basic etch-a-sketch

--- script.js
+++ script.js
@@ -3,51 +3,66 @@ const DEFAULT_COLOR = "white";
 const DEFAULT_PICKER_COLOR = "black";
 const DEFAULT_SIZE = 16;
 
-
-
-let currentColor = DEFAULT_COLOR;
+let currentPickerColor = DEFAULT_PICKER_COLOR;
 let currentSize = DEFAULT_SIZE;
 
-
-
 const grid = document.getElementById('grid');
-const colorPicker = document.getElementById('color-picker');
 const slider = document.getElementById('slider');
-
-//getting size from user
-// slider.addEventListener('input', function(e) {
-//     console.log(e.target.value);
-//     setupGrid(e.target.value, e.target.value);
-// })
-
-// getting color from user
-colorPicker.addEventListener('input', function(e) {
-    currentColor = e.target.value;
-})
+const colorPicker = document.getElementById('color-picker');
+const sliderValue = document.getElementById('slider-value');
+const rainbowBtn = document.getElementById('rainbow-button');
+const clearBtn = document.getElementById('clear-button');
 
 
+// event listeners
+slider.oninput = (e) => updateSize(e.target.value);
+colorPicker.oninput = (e) => updateColor(e.target.value);
+
+
+// update size
+function updateSize(newSize) {
+    currentSize = newSize;
+    sliderValue.innerHTML = `${currentSize} x ${currentSize}`
+    clearGrid();
+    setupGrid(currentSize);
+}
+
+// update color
+function updateColor(newColor) {
+    currentPickerColor = newColor;
+}
+
+// clear grid 
+function clearGrid() {
+    grid.innerHTML = "";
+}
 
 // setup grid
-function setupGrid(rows, cols) {
-    grid.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-    for (i = 0; i < rows * cols; i++) {
-        const gridItem = document.createElement('div');
+function setupGrid(num) {
+    grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${num}, 1fr)`
+    for (i = 0; i < num * num; i++) {
+        let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        gridItem.style.background = currentColor;
+        gridItem.style.background = DEFAULT_COLOR;
         gridItem.addEventListener('mouseover', colorSquare);
         grid.appendChild(gridItem);
     }
 }
 
-setupGrid(64, 64);
-
-
-
 // change the color of each square
 function colorSquare(e) {
-    e.target.style.background = currentColor;
-
+    e.target.style.background = currentPickerColor;
 }
+
+
+// on load
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
+}
+
+
+
+
 
 
